d715b13bd0bdfea1be4298e2448163c9e234277c
Travis CI User <travis@example.org>
2020-07-30 12:19:11 +0000

    Updates

---
+++ component---src-pages-usingtailwind-js-8093763618ee07a44216.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{OEcT:function(t,n,e){},r3ZI:function(t,n,e){"use strict";e.r(n);var c=e("q1tI"),a=e.n(c);e("OEcT");n.default=function(){return a.a.createElement("h1",{className:"text-3xl"},"This is a 3xl text")}}}]);
+//# sourceMappingURL=component---src-pages-usingtailwind-js-8093763618ee07a44216.js.map
